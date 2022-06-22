462bbd3a4afee3769912e1924212400886dbbc77
Tauheed Issa <tauheedessa@gmail.com>
2022-06-23 00:52:11 +0500

    Add files via upload

---
+++ css/index.js
@@ -0,0 +1,12 @@
+// // Activate Carousel
+// $("#carouselExampleIndicators").carousel();
+//
+// // Enable Carousel Indicators
+// $(".carousel-item").click(function(){
+//   $("#carouselExampleIndicators").carousel(1);
+// });
+//
+// // Enable Carousel Controls
+// $(".carousel-control-prev").click(function(){
+//   $("#carouselExampleIndicators").carousel("prev");
+// });
